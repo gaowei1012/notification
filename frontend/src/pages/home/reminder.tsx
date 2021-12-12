438548c6396061994e9d289ba904ef3c3e46273b
gaowei1012 <gyb2020018@163.com>
2021-12-12 17:10:48 +0800

    fix reminder

--- frontend/src/pages/home/reminder.tsx
+++ frontend/src/pages/home/reminder.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, SafeAreaView, Text, TouchableOpacity, Image } from 'react-native'
+import { View, SafeAreaView, Text } from 'react-native'
+import { IReminderProps } from '../../types/reminder'
 import { styles } from '../../styles/reminder'
 import { TopNavigationBar } from '../../utils'
 import GoBack from '../../navigation/GoBack'
 
-const Reminder = () => {
+const Reminder: React.FC<IReminderProps> = () => {
   return (
     <SafeAreaView style={styles.container}>
       <TopNavigationBar leftButton={<GoBack/> } title='添加提醒' />
