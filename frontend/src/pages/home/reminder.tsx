4cb8a3389ff4cec3467ec902f77a22fd0ab1fae4
gaowei1012 <gyb2020018@163.com>
2021-12-12 17:45:05 +0800

    update reminder

--- frontend/src/pages/home/reminder.tsx
+++ frontend/src/pages/home/reminder.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { View, SafeAreaView, Text } from 'react-native'
+import { View, SafeAreaView, Text, TextInput } from 'react-native'
 import { IReminderProps } from '../../types/reminder'
+import { back, TopNavigationBar } from '../../utils'
+import { Button } from 'react-native-elements'
 import { styles } from '../../styles/reminder'
-import { TopNavigationBar } from '../../utils'
 import GoBack from '../../navigation/GoBack'
 
 const Reminder: React.FC<IReminderProps> = () => {
   return (
     <SafeAreaView style={styles.container}>
       <TopNavigationBar leftButton={<GoBack/> } title='添加提醒' />
-      <View>
-        <Text>添加提醒</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.contentTitle}>提醒标题:</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder='请输入事项标题'
+        />
       </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.contentTitle}>提醒内容:</Text>
+        <TextInput
+          style={styles.multilineTextInput}
+          placeholder='请输入事项内容'
+          multiline={true}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.contentTitle}>开始时间:</Text>
+        
+      </View>
+      <Button activeOpacity={.8} onPress={() => back()} style={styles.confirm} title='添加'/>
     </SafeAreaView>
   )
 }
--- frontend/src/styles/reminder.ts
+++ frontend/src/styles/reminder.ts
@@ -7,11 +7,44 @@ const { flex, defaultBackgroundColor } = commomStyles
 const styles = StyleSheet.create({
   container: {
     ...flex,
-    ...defaultBackgroundColor
+    ...defaultBackgroundColor,
   },
   back: {
     width: px2dp(18),
     height: px2dp(18)
+  },
+  contentTitle: {
+    marginLeft: px2dp(14),
+    fontWeight: '600',
+    color: '#8d96a0'
+  },
+  textInput: {
+    width: px2dp(350),
+    // height: px2dp(10),
+    alignSelf: 'center',
+    marginTop:px2dp(6),
+    borderRadius: px2dp(6),
+    borderBottomWidth: px2dp(1),
+    borderBottomColor: '#ddd',
+    padding: px2dp(8)
+  },
+  multilineTextInput: {
+    width: px2dp(350),
+    height: px2dp(120),
+    alignSelf: 'center',
+    marginTop:px2dp(12),
+    borderRadius: px2dp(6),
+    borderWidth: px2dp(1),
+    borderColor: '#ddd',
+    padding: px2dp(8)
+  },
+  inputContainer: {
+    marginVertical: px2dp(10)
+  },
+  confirm: {
+    width: px2dp(350),
+    alignSelf: 'center',
+    marginTop: px2dp(120)
   }
 })
 
