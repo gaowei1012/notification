cc65c41bd58de23b7f07834a0da8fc64ddb29cbd
gaowei1012 <gyb2020018@163.com>
2021-12-14 16:23:11 +0800

    fix

--- frontend/src/navigation/screen/homeScreen.tsx
+++ frontend/src/navigation/screen/homeScreen.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
+import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../../pages/home/home'
 
 const Stack = createNativeStackNavigator()
 
-const HomeScreen = () => {
+const HomeScreen: React.FC<{}> = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name='home' component={Home} />
--- frontend/src/navigation/screen/settingScreen.tsx
+++ frontend/src/navigation/screen/settingScreen.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
+import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Settings from '../../pages/setting/setting'
 
 const Stack = createNativeStackNavigator()
 
-const SettingScreen = () => {
+const SettingScreen: React.FC<{}> = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name='setting' component={Settings} />
